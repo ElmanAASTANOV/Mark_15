8971c47a324b47ae2195eac4ed9130b39401a067
Elman Astanov <elman.astanov@gmail.com>
2018-01-03 12:03:45 +0400

    Empty

---
+++ ClinetSide/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClinetSide
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
